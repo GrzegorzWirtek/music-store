c2776de83456017abbadda8505ce65f8aab1df3d
GrzegorzWirtek <grzegorz.wirtek@wp.pl>
2022-11-05 16:44:30 +0100

    change navigation to responsive

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,4 +1,6 @@
 import './Navbar.scss';
+import { useState } from 'react';
+import Ham from './Ham/Ham';
 import { Link, useNavigate } from 'react-router-dom';
 import { useAppSelector } from '../../app/hooks';
 import { useAppDispatch } from '../../app/hooks';
@@ -17,24 +19,40 @@ const Navbar = () => {
 		navigate('/admin');
 	};
 
+	const [hamActive, setHamActive] = useState(false);
+
+	const toggleHam = () => {
+		setHamActive((prev) => !prev);
+	};
+
+	const closeHam = () => {
+		setHamActive(false);
+	};
+
 	return (
 		<>
-			<Link className='navbar__link' to='/'>
-				Home
-			</Link>
-			<Link className='navbar__link' to='/shop'>
-				Shop
-			</Link>
-			<Link className='navbar__link' to='/contact'>
-				Contact
-			</Link>
-			<Link className='navbar__link' to='/admin'>
-				Admin
-			</Link>
-			{login && password ? (
-				<button onClick={handleLogout}>Logout</button>
-			) : null}
-
+			<Ham hamActive={hamActive} toggleHam={toggleHam} />
+			<nav
+				onClick={closeHam}
+				className={`navbar ${hamActive ? 'navbar--active' : ''}`}>
+				<Link className='navbar__link' to='/'>
+					Home
+				</Link>
+				<Link className='navbar__link' to='/shop'>
+					Shop
+				</Link>
+				<Link className='navbar__link' to='/contact'>
+					Contact
+				</Link>
+				<Link className='navbar__link' to='/admin'>
+					Admin
+				</Link>
+				{login && password ? (
+					<button onClick={handleLogout} className='navbar__link'>
+						Logout
+					</button>
+				) : null}
+			</nav>
 			<Link className='navbar__link navbar__link--cart' to='/cart'>
 				<img src='cart.png' alt='cart icon' className='navbar__img' />
 				<p className='navbar__cart-number'>{cart.length}</p>
