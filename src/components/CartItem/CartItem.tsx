dedb1d3d65443f0160c4d44a93fa496483f3cf07
GrzegorzWirtek <grzegorz.wirtek@wp.pl>
2022-11-06 14:24:52 +0100

    styling cart item

---
+++ src/components/CartItem/CartItem.tsx
@@ -0,0 +1,48 @@
+import './CartItem.scss';
+import { CartContentType } from '../../features/CartContent/CartContent';
+
+type CartItemProps = {
+	product: CartContentType;
+	increase: (_id: string) => void;
+	decrease: (_id: string) => void;
+	remove: (_id: string) => void;
+};
+
+const CartItem = ({
+	product: { _id, name, price, productsInTheCart, imageBase64 },
+	increase,
+	decrease,
+	remove,
+}: CartItemProps) => {
+	return (
+		<div className='cart-item'>
+			<div className='cart-item__img-wrapper'>
+				<img className='cart-item__img' src={imageBase64} alt={name} />
+			</div>
+
+			<h2 className='cart-item__name'>{name}</h2>
+			<p className='cart-item__price'>{price}$</p>
+
+			<div className='cart-item__nr-of-wrapper'>
+				<p className='cart-item__nr-of-title'>Quanity</p>
+				<button
+					className='cart-item__btn cart-item__nr-of-item'
+					onClick={() => decrease(_id)}>
+					-
+				</button>
+				<p className='cart-item__number cart-item__nr-of-item'>
+					{productsInTheCart}
+				</p>
+				<button
+					className='cart-item__btn cart-item__nr-of-item'
+					onClick={() => increase(_id)}>
+					+
+				</button>
+			</div>
+			<button className='cart-item__remove-btn' onClick={() => remove(_id)}>
+				Remove from cart
+			</button>
+		</div>
+	);
+};
+export default CartItem;
--- src/features/CartContent/CartContent.tsx
+++ src/features/CartContent/CartContent.tsx
@@ -1,3 +1,4 @@
+import './CartContent.scss';
 import { useEffect } from 'react';
 import { useAppDispatch, useAppSelector } from '../../app/hooks';
 import {
@@ -6,14 +7,15 @@ import {
 	decreaseNrOfProducts,
 } from './CartContentSlice';
 import { fetchProducts } from '../Products/ProductsSlice';
+import CartItem from '../../components/CartItem/CartItem';
 
-type CartContentType = {
+export type CartContentType = {
 	_id: string;
 	name: string;
 	productsInTheCart: number;
 	price: number;
 	imageBase64: string;
-}[];
+};
 
 const CartContent = () => {
 	const { cart } = useAppSelector((state) => state.cart);
@@ -25,42 +27,6 @@ const CartContent = () => {
 			dispatch(fetchProducts());
 		}
 	}, [dispatch, products.length]);
-
-	const productsInTheCart = () => {
-		const arr = [] as CartContentType;
-		for (const a of products) {
-			for (const b of cart) {
-				if (a._id === b._id) {
-					const product = {
-						_id: a._id,
-						name: a.name,
-						productsInTheCart: b.productsInTheCart,
-						price: a.price * b.productsInTheCart,
-						imageBase64: a.imageBase64,
-					};
-					arr.push(product);
-				}
-			}
-		}
-		return arr;
-	};
-
-	const productContent = productsInTheCart().map((product) => (
-		<div key={product._id}>
-			<img src={product.imageBase64} alt={product.name} />
-			<p>sum: {product.price}</p>
-			<p>
-				{product.name} id: {product._id} nrOf: {product.productsInTheCart}
-			</p>
-			<button onClick={() => handleRemoveProduct(product._id)}>remove</button>
-			<button onClick={() => handleIncreaseNrOfProducts(product._id)}>
-				increase
-			</button>
-			<button onClick={() => handleDecreaseNrOfProducts(product._id)}>
-				decrease
-			</button>
-		</div>
-	));
 
 	const handleRemoveProduct = (id: string) => {
 		dispatch(removeFromCart(id));
@@ -74,7 +40,45 @@ const CartContent = () => {
 		dispatch(decreaseNrOfProducts(id));
 	};
 
-	return <>{productContent}</>;
+	const productsInTheCart = () => {
+		const arr = [] as CartContentType[];
+		for (const a of products) {
+			for (const b of cart) {
+				if (a._id === b._id) {
+					const product = {
+						_id: a._id,
+						name: a.name,
+						productsInTheCart: b.productsInTheCart,
+						price: a.price,
+						imageBase64: a.imageBase64,
+					};
+					arr.push(product);
+				}
+			}
+		}
+		return arr;
+	};
+
+	const productContent = productsInTheCart().map((product) => (
+		<CartItem
+			key={product._id}
+			product={product}
+			increase={handleIncreaseNrOfProducts}
+			decrease={handleDecreaseNrOfProducts}
+			remove={handleRemoveProduct}
+		/>
+	));
+
+	const emptyMessage = (
+		<p className='cart-content__empty'>Shopping cart is empty</p>
+	);
+
+	return (
+		<section className='cart-content'>
+			<h2 className='cart-content__title'>Shopping cart</h2>
+			{productContent.length ? productContent : emptyMessage}
+		</section>
+	);
 };
 
 export default CartContent;
