ffe5b9e3a8b7c9b63f648d07783369a2a065fdfc
GrzegorzWirtek <grzegorz.wirtek@wp.pl>
2023-02-12 18:54:39 +0100

    change styles and add hiding keyboard on submit for mobile devices

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,4 +1,4 @@
-import { memo } from 'react';
+import { memo, useRef } from 'react';
 import './Search.scss';
 
 type SearchProps = {
@@ -8,19 +8,27 @@ type SearchProps = {
 };
 
 const Search = ({ searchedElement, search, cleanSearch }: SearchProps) => {
+	const inputRef = useRef<HTMLInputElement>(null!);
+
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		inputRef.current.blur();
+	};
+
 	return (
-		<div className='search'>
+		<form className='search' onSubmit={handleSubmit}>
 			<input
 				className='search__input'
 				type='text'
 				onChange={(e) => search(e)}
 				value={searchedElement}
 				placeholder='Search'
+				ref={inputRef}
 			/>
 			<button className='search__btn' type='button' onClick={cleanSearch}>
 				<img className='search__svg' src='close.svg' alt='close' />
 			</button>
-		</div>
+		</form>
 	);
 };
 
--- src/pages/ProductInfo/ProductInfo.tsx
+++ src/pages/ProductInfo/ProductInfo.tsx
@@ -1,12 +1,16 @@
-import { useParams } from 'react-router-dom';
-import { useAppSelector } from '../../app/hooks';
-// import Modal from '../../components/Modal/Modal';
+import { useState } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import { addToCart } from '../../features/CartContent/CartContentSlice';
+import Modal from '../../components/Modal/Modal';
 
 const ProductInfo = () => {
+	const [modalVisible, setModalVisible] = useState<string | boolean>(false);
 	const { products } = useAppSelector((state) => state.products);
+	const dispatch = useAppDispatch();
+	const navigate = useNavigate();
 	const { id } = useParams();
 	const p = products.filter((item) => item._id === id);
-	console.log(p);
 	const {
 		_id,
 		imageBase64,
@@ -16,6 +20,27 @@ const ProductInfo = () => {
 		productsInTheShop,
 	} = p[0];
 
+	const handleAddToCart = () => {
+		const product = {
+			_id,
+			name,
+			price,
+			productsInTheCart,
+			productsInTheShop,
+		};
+		dispatch(addToCart(product));
+		setModalVisible(name);
+	};
+
+	const handleGoToCart = () => {
+		setModalVisible(false);
+		navigate('../../cart');
+	};
+
+	const handleContinueShopping = () => {
+		setModalVisible(false);
+	};
+
 	return (
 		<section className='product-info'>
 			<img className='product-info__img' src={imageBase64} alt={name} />
@@ -23,19 +48,18 @@ const ProductInfo = () => {
 			<p className='product-info__price'>{price}</p>
 			<p className='product-info__products-in-the-cart'>{productsInTheCart}</p>
 			<p className='product-info__products-in-the-shop'>{productsInTheShop}</p>
-			<button
-				onClick={() =>
-					console.log({
-						_id,
-						name,
-						price,
-						productsInTheCart,
-						productsInTheShop,
-					})
-				}
-				className='product__btn'>
+			<button onClick={() => handleAddToCart()} className='product__btn'>
 				ADD TO CART
 			</button>
+			{modalVisible && (
+				<Modal
+					clickOne={handleGoToCart}
+					clickTwo={handleContinueShopping}
+					modalTitle={`"${modalVisible}" added to shopping cart`}
+					clickOneText='Go to shopping cart'
+					clickTwoText='Back to shopping'
+				/>
+			)}
 		</section>
 	);
 };
