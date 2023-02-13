882e536c8b79d1dd9b5713da692779c384295283
GrzegorzWirtek <grzegorz.wirtek@wp.pl>
2023-02-13 17:28:26 +0100

    style change

--- src/hooks/useGetProductById.ts
+++ src/hooks/useGetProductById.ts
@@ -3,6 +3,7 @@ import { useAppSelector } from '../app/hooks';
 export const useGetProductById = (id: string) => {
 	const { products } = useAppSelector((state) => state.products);
 
+	if (!products.length) return null;
 	const product = products.find((item) => item._id === id);
 
 	return product;
